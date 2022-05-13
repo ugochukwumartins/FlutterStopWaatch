4175db8cfe54199bdec287f8bdb5ddb3181578b7
ugochukwumartins <uokorocha72@gmail.com>
2022-05-13 16:13:27 +0100

    added start and stop button

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+import 'package:stopwatch_flutter/ui/reset_button.dart';
+import 'package:stopwatch_flutter/ui/start_stop_button.dart';
 import 'package:stopwatch_flutter/ui/stopwatch_renderer.dart';
 
 class Stopwatch extends StatefulWidget {
@@ -13,18 +15,21 @@ class Stopwatch extends StatefulWidget {
 
 class _StopwatchState extends State<Stopwatch>
     with SingleTickerProviderStateMixin {
+  Duration _previouslyElasped = Duration.zero;
+  Duration _currentlyElasped = Duration.zero;
+  bool _isRunning = false;
   late final Ticker _ticker;
-  Duration _elasped = Duration.zero;
+  Duration get _elasped => _previouslyElasped + _currentlyElasped;
   @override
   void initState() {
     super.initState();
 
     _ticker = this.createTicker((elapsed) {
       setState(() {
-        _elasped = elapsed;
+        _currentlyElasped = elapsed;
       });
     });
-    _ticker.start();
+    //_ticker.start();
   }
 
   @override
@@ -33,13 +38,58 @@ class _StopwatchState extends State<Stopwatch>
     super.dispose();
   }
 
+  void _toggleRunning() {
+    setState(() {
+      _isRunning = !_isRunning;
+      if (_isRunning) {
+        _ticker.start();
+      } else {
+        _ticker.stop();
+        _previouslyElasped += _currentlyElasped;
+        _currentlyElasped = Duration.zero;
+      }
+    });
+  }
+ void _reset() {
+     _ticker.stop();
+    setState(() {
+      
+      _isRunning = false;
+        _previouslyElasped    = Duration.zero;
+           _currentlyElasped = Duration.zero;
+     
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constrain) {
       final radius = constrain.maxWidth / 2;
-      return StopWatchRenderer(
-        elapsed: _elasped,
-        radius:radius,
+      return Stack(
+        children: [
+          StopWatchRenderer(
+            elapsed: _elasped,
+            radius: radius,
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: ResetButton(
+                onPressed: _reset,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: StartStopButton(
+                  isRunning: _isRunning, onPressed: _toggleRunning),
+            ),
+          )
+        ],
       );
     });
   }
